ed6eae5c25f3f63e730d2b3d0917d79aee01d821
Mochalov Maksim <mmochalov@usetech.ru>
2025-01-20 14:00:33 +0300

    fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { QueryClientProvider} from 'react-query'
 import queryClient from '@/queries/client'
 import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AppProvider } from './contexts/appProvider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <AppProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>   
+        </HashRouter>   
       </AppProvider>
     </QueryClientProvider>
   </React.StrictMode>,
